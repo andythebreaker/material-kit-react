bbf6fe92006ab364a6c4ff08fc588337d3fd8487
andythebreaker <e24074724@mail.ncku.edu.tw>
2022-12-08 22:21:33 +0800

    https://raw.githubusercontent.com/andythebreaker/yolov5ver7FrontEndDocker/main/inject.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default function App() {
   }, [pathname]);
 
   const status1 = useScript("https://cdn.jsdelivr.net/npm/sweetalert2@11");
-  const status2 = useScript("http://192.168.0.101:62471/inject.js");
+  const status2 = useScript("https://raw.githubusercontent.com/andythebreaker/yolov5ver7FrontEndDocker/main/inject.js");//("http://192.168.0.101:62471/inject.js");
 
   const getRoutes = (allRoutes) =>
     allRoutes.map((route) => {
